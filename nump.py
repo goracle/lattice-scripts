82a7f82cb6673fbb125203e56b4accb81379e7cf
Dan H <goracle@users.noreply.github.com>
2017-11-29 12:09:50 -0500

    Add html printing of energy coverage table.

--- nump.py
+++ nump.py
@@ -7,34 +7,45 @@ from math import pi, sqrt
 from warnings import warn
 from tabulate import tabulate
 
+
 #relative momentum cut on == True,
 #set to False reproduces the number of momentum combinations we saw for pmax=2:
 #27959
 relmomcut=True
 debug=False
+html=True
+if html:
+    extra="<br />"
+else:
+    extra=''
 
 def pipiEnergy(nump1, nump2):
     """Return pipi energy in GEV"""
-    warn("Neglecting pipi interaction energy")
+    warn("Neglecting pipi interaction energy"+extra)
     energy = reten(nump1)+reten(nump2)
-    rete = str(round(energy, 3))
-    if len(rete) < 5:
-        rete = rete + '0'
+    ecm = sqrt(energy**2-(2*pi/24)**2*norm2(nump1+nump2))
+    rete = str(round(ecm, 3))
     return rete
 
 def reten(nump):
     """Do the numerics for the energy"""
-    warn("Assuming volume is 24c.")
+    warn("Assuming volume is 24c."+extra)
     onedv = 24
-    return sqrt((.139570)**2+(2*pi/onedv)**2*np.inner(nump,nump))
+    return sqrt((.139570)**2+(2*pi/onedv)**2*norm2(nump))
 
 def main(maxpc, maxpr):
+    if html:
+        print("<p>")
     print("Assuming max pipi relative momentum is", maxpr)
+    if html:
+        print("<br />")
     count=0
     plist = []
     eset = set()
     nmom=int(sys.argv[1])
     print("Assuming max pipi center of mass momentum is "+str(maxpc))
+    if html:
+        print("<br />")
     maxpn = nmom
     energydict = defaultdict(lambda: 0)
     for i in range(-maxpn,maxpn+1):
@@ -46,8 +57,12 @@ def main(maxpc, maxpr):
                     count+=1
                     plist.append([i,j,k])
     print("Number of single particle momenta combinations=",count)
+    if html:
+        print("<br />")
     #3 rho polarizations + 1 sigma + 1 pion = num of species
     print("Number of meson fields needed=",count*5)
+    if html:
+        print("<br />")
     plist = np.array(plist)
     #now count the number of pipi momentum combinations
     count = 0
@@ -86,6 +101,8 @@ def main(maxpc, maxpr):
                 eset.add(float(esnk))
                 energydict[ekey] += 5
     print("Number of pipi momentum combinations=", count)
+    if html:
+        print("<br />")
     count+=3*len(plist) #pionChk, pion, sigma correlator
     count+=6*len(plist) #rho correlator (aux cut, so not 9)
     count+=len(plist) #sigma bubbles
@@ -103,18 +120,32 @@ def main(maxpc, maxpr):
             count+=1 #T sigma sink
             count+=2*3 #rho sink
     print("Number of momentum combinations=", count)
+    if html:
+        print("<br />")
     elist = convSet(eset)
     emat = makemat(elist, energydict)
     #energyPrint(energydict)
     goodEprint(emat, elist)
+    if html:
+        print("</p>")
 
 def goodEprint(emat, elist):
     """Pretty print of energy data""" 
     relist = np.array([elist]).T
+    print(elist)
     #len1 = len(emat)
     #pmat = np.zeros((len1, len1+1), dtype=object)
     pmat = np.concatenate((relist,emat), axis=1)
-    table = tabulate(pmat, elist, tablefmt="fancy_grid")
+    if html:
+        table = tabulate(pmat, elist, tablefmt="html")
+    else:
+        table = tabulate(pmat, elist, tablefmt="fancy_grid")
+    print("Printing statistical energy (GeV) coverage")
+    if html:
+        print("<br />")
+    print("entries are number of momentum combinations with that COM energy")
+    if html:
+        print("<br />")
     print(table)
 
 
@@ -131,7 +162,12 @@ def makemat(elist, energydict):
 def convSet(eset):
     """convert energy set to list for cols/rows"""
     preret = sorted(list(eset))
-    ret = [str(i) for i in preret]
+    preret = [str(i) for i in preret]
+    ret = []
+    for i in preret:
+        if len(i) < 5:
+            i = i + '0'*(5-len(i))
+        ret.append(i)
     return ret
 
 def energyPrint(elist):
