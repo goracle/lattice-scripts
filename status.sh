3e4a2e9745f8fd8dd2f930a79157e8b6e106e940
Dan H <goracle@users.noreply.github.com>
2018-03-12 11:31:51 -0400

    Add changes to slurm productivity scripts.

--- status.sh
+++ status.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 #calls less on the slurm output of a job by id
-dir='run_new16c'
+if [ "$2" = "" ]; then
+dir='prod_24c'
+else
+dir="$2"
+fi
 a=$(mktemp)
-cd ~/cps/cps_pp/work/pion2pt && \
+cd /hpcgpfs01/scratch/dsh/16c_regression_daiqian_stat_check && \
 find $(pwd) -path "*$1*" | tee $a
 num=$(cat $a | wc -l)
 if [ $num -eq 1 ]; then
+if [  "$2" = "" ]; then
 less $(cat $a)
 else
+tail -n 30 $(cat $a)
+fi
+else
+
 cd /hpcgpfs01/scratch/dsh/$dir && \
 find $(pwd) -path "*$1*" | tee $a
 num=$(cat $a | wc -l)
 if [ $num -eq 1 ]; then
+if [  "$2" = "" ]; then
 less $(cat $a)
+else
+tail -n 30 $(cat $a)
+fi
 fi
 fi
