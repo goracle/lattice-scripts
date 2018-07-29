af5c24ab73cc911e128cdabafdae18a37ca8c7a8
Dan H <goracle@users.noreply.github.com>
2018-07-29 19:18:53 -0400

    Update slat.sh with comments.

--- slat.sh
+++ slat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#pick out the latest slurm scritp from the current working directory (latest means largest run number
+#pick out the latest slurm script from the current working directory (latest means largest run number)
+#alias this to 'slat', I use this all the time
 
 for file in $( ls slurm*out | sort -h); do 
 a=$file
